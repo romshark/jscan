bd6cd6248e4b069694914f1c2023d60d7d53b856
Roman Sharkov <roman.scharkov@gmail.com>
2023-06-18 21:40:50 +0200

    feat: Move comparison benchmarks to separate repo
    Avoid unnecessary dependencies by moving all
    comparison benchmarks to a different repository.

--- bench_test.go
+++ bench_test.go
@@ -1,7 +1,6 @@
 package jscan_test
 
 import (
-	"bytes"
 	"compress/gzip"
 	"encoding/json"
 	"fmt"
@@ -13,15 +12,7 @@ import (
 
 	"github.com/romshark/jscan/v2"
 
-	encodingjson "encoding/json"
-
-	bytedancesonic "github.com/bytedance/sonic"
-	gofasterjx "github.com/go-faster/jx"
-	goccygojson "github.com/goccy/go-json"
-	jsoniter "github.com/json-iterator/go"
 	"github.com/stretchr/testify/require"
-	tidwallgjson "github.com/tidwall/gjson"
-	valyalafastjson "github.com/valyala/fastjson"
 )
 
 type Stats struct {
@@ -77,206 +68,7 @@ func MustCalcStatsJscan(p *jscan.Parser[[]byte], str []byte) (s Stats) {
 	return
 }
 
-func MustCalcStatsJsoniter(p *jsoniter.Iterator, str []byte) (s Stats) {
-	p.ResetBytes(str)
-	var readValue func(lv int, k string, ai int, i *jsoniter.Iterator)
-	readValue = func(
-		level int,
-		key string,
-		arrIndex int,
-		i *jsoniter.Iterator,
-	) {
-		if level > s.MaxDepth {
-			s.MaxDepth = level
-		}
-		if l := len(key); l > 0 {
-			s.TotalKeys++
-			if l > s.MaxKeyLen {
-				s.MaxKeyLen = l
-			}
-		}
-		switch i.WhatIsNext() {
-		case jsoniter.StringValue:
-			i.ReadString()
-			s.TotalStrings++
-		case jsoniter.NumberValue:
-			i.ReadNumber()
-			s.TotalNumbers++
-		case jsoniter.NilValue:
-			i.ReadNil()
-			s.TotalNulls++
-		case jsoniter.BoolValue:
-			i.ReadBool()
-			s.TotalBooleans++
-		case jsoniter.ArrayValue:
-			s.TotalArrays++
-			l := level + 1
-			index := 0
-			for e := i.ReadArray(); e; e = i.ReadArray() {
-				readValue(l, "", index, i)
-				index++
-				if index > s.MaxArrayLen {
-					s.MaxArrayLen = index
-				}
-			}
-		case jsoniter.ObjectValue:
-			s.TotalObjects++
-			l := level + 1
-			for f := i.ReadObject(); f != ""; f = i.ReadObject() {
-				readValue(l, f, -1, i)
-			}
-		}
-	}
-	readValue(0, "", -1, p)
-	return
-}
-
-func MustCalcStatsGofasterJx(p *gofasterjx.Decoder, str []byte) (s Stats) {
-	p.ResetBytes(str)
-	var jxParseValue func(lv int, k []byte, ai int) error
-	jxParseValue = func(
-		level int,
-		key []byte,
-		arrayIndex int,
-	) error {
-		if level > s.MaxDepth {
-			s.MaxDepth = level
-		}
-		if l := len(key); l > 0 {
-			s.TotalKeys++
-			if l > s.MaxKeyLen {
-				s.MaxKeyLen = l
-			}
-		}
-
-		switch p.Next() {
-		case gofasterjx.String:
-			s.TotalStrings++
-			if err := p.Skip(); err != nil {
-				return err
-			}
-		case gofasterjx.Null:
-			s.TotalNulls++
-			if err := p.Skip(); err != nil {
-				return err
-			}
-		case gofasterjx.Bool:
-			s.TotalBooleans++
-			if err := p.Skip(); err != nil {
-				return err
-			}
-		case gofasterjx.Number:
-			s.TotalNumbers++
-			if err := p.Skip(); err != nil {
-				return err
-			}
-		case gofasterjx.Array:
-			s.TotalArrays++
-			i := 0
-			if err := p.Arr(func(d *gofasterjx.Decoder) error {
-				if err := jxParseValue(level+1, nil, i); err != nil {
-					return err
-				}
-				i++
-				if i > s.MaxArrayLen {
-					s.MaxArrayLen = i
-				}
-				return nil
-			}); err != nil {
-				return err
-			}
-		case gofasterjx.Object:
-			s.TotalObjects++
-			if err := p.ObjBytes(func(d *gofasterjx.Decoder, key []byte) error {
-				return jxParseValue(level+1, key, -1)
-			}); err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-	if err := jxParseValue(0, nil, -1); err != nil {
-		panic(err)
-	}
-	return
-}
-
-func MustCalcStatsValyalaFastjson(p *valyalafastjson.Parser, str []byte) (s Stats) {
-	v, err := p.ParseBytes(str)
-	if err != nil {
-		panic(err)
-	}
-
-	var parseValue func(v *valyalafastjson.Value, lv int, k []byte, a int) error
-	parseValue = func(
-		v *valyalafastjson.Value,
-		level int,
-		key []byte,
-		arrayIndex int,
-	) error {
-		if level > s.MaxDepth {
-			s.MaxDepth = level
-		}
-		if l := len(key); l > 0 && arrayIndex == -1 {
-			s.TotalKeys++
-			if l > s.MaxKeyLen {
-				s.MaxKeyLen = l
-			}
-		}
-
-		switch v.Type() {
-		case valyalafastjson.TypeString:
-			s.TotalStrings++
-
-		case valyalafastjson.TypeNull:
-			s.TotalNulls++
-
-		case valyalafastjson.TypeTrue, valyalafastjson.TypeFalse:
-			s.TotalBooleans++
-
-		case valyalafastjson.TypeNumber:
-			s.TotalNumbers++
-
-		case valyalafastjson.TypeArray:
-			s.TotalArrays++
-			values, err := v.Array()
-			if err != nil {
-				return err
-			}
-			lv := level + 1
-			for i, v := range values {
-				if err := parseValue(v, lv, key, i); err != nil {
-					return err
-				}
-				if i := i + 1; i > s.MaxArrayLen {
-					s.MaxArrayLen = i
-				}
-			}
-			return nil
-
-		case valyalafastjson.TypeObject:
-			s.TotalObjects++
-			o, err := v.Object()
-			if err != nil {
-				return err
-			}
-			lv := level + 1
-			o.Visit(func(key []byte, v *valyalafastjson.Value) {
-				if err = parseValue(v, lv, key, -1); err != nil {
-					return
-				}
-			})
-			return nil
-		}
-		return nil
-	}
-	if err := parseValue(v, 0, nil, -1); err != nil {
-		panic(err)
-	}
-	return
-}
-
-func TestImplementations(t *testing.T) {
+func TestCalcStats(t *testing.T) {
 	const input = `{
 		"s":"value",
 		"t":true,
@@ -310,23 +102,8 @@ func TestImplementations(t *testing.T) {
 		MaxArrayLen:   5,
 	}
 
-	t.Run("jscan", func(t *testing.T) {
-		p := jscan.NewParser[[]byte](64)
-		require.Equal(t, expect, MustCalcStatsJscan(p, []byte(input)))
-	})
-	t.Run("jsoniter", func(t *testing.T) {
-		p := jsoniter.NewIterator(jsoniter.ConfigFastest)
-		require.Equal(t, expect, MustCalcStatsJsoniter(p, []byte(input)))
-	})
-	t.Run("gofaster-jx", func(t *testing.T) {
-		p := new(gofasterjx.Decoder)
-		require.Equal(t, expect, MustCalcStatsGofasterJx(p, []byte(input)))
-	})
-
-	t.Run("valyala-fastjson", func(t *testing.T) {
-		p := new(valyalafastjson.Parser)
-		require.Equal(t, expect, MustCalcStatsValyalaFastjson(p, []byte(input)))
-	})
+	p := jscan.NewParser[[]byte](64)
+	require.Equal(t, expect, MustCalcStatsJscan(p, []byte(input)))
 }
 
 var gs Stats
@@ -351,75 +128,22 @@ func BenchmarkCalcStats(b *testing.B) {
 			src, err := bd.input.GetJSON()
 			require.NoError(b, err)
 
-			b.Run("jscan___________", func(b *testing.B) {
-				p := jscan.NewParser[[]byte](1024)
-				b.ResetTimer()
-				for i := 0; i < b.N; i++ {
-					gs = MustCalcStatsJscan(p, src)
-				}
-			})
-
-			b.Run("jsoniter________", func(b *testing.B) {
-				p := jsoniter.NewIterator(jsoniter.ConfigFastest)
-				b.ResetTimer()
-				for i := 0; i < b.N; i++ {
-					gs = MustCalcStatsJsoniter(p, src)
-				}
-			})
-
-			b.Run("gofaster-jx_____", func(b *testing.B) {
-				p := new(gofasterjx.Decoder)
-				b.ResetTimer()
-				for i := 0; i < b.N; i++ {
-					gs = MustCalcStatsGofasterJx(p, src)
-				}
-			})
-
-			b.Run("valyala-fastjson", func(b *testing.B) {
-				p := new(valyalafastjson.Parser)
-				b.ResetTimer()
-				for i := 0; i < b.N; i++ {
-					gs = MustCalcStatsValyalaFastjson(p, src)
-				}
-			})
+			p := jscan.NewParser[[]byte](1024)
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				gs = MustCalcStatsJscan(p, src)
+			}
 		})
 	}
 }
 
 func TestValid(t *testing.T) {
 	j := `[false,[[2, {"[foo]":[{"bar-baz":"fuz"}]}]]]`
-	require.True(t, encodingjson.Valid([]byte(j)))
-
-	t.Run("jscan", func(t *testing.T) {
-		require.True(t, jscan.Valid(j))
-	})
-
-	t.Run("jscan_verifier", func(t *testing.T) {
-		require.True(t, jscan.Valid(j))
-	})
-
-	t.Run("jsoniter", func(t *testing.T) {
-		require.True(t, jsoniter.Valid([]byte(j)))
-	})
-
-	t.Run("tidwallgjson", func(t *testing.T) {
-		require.True(t, tidwallgjson.Valid(j))
-	})
-
-	t.Run("fast-json", func(t *testing.T) {
-		require.NoError(t, valyalafastjson.Validate(j))
-	})
-
-	t.Run("goccy-go-json", func(t *testing.T) {
-		require.True(t, goccygojson.Valid([]byte(j)))
-	})
-
-	t.Run("bytedance-sonic", func(t *testing.T) {
-		require.True(t, bytedancesonic.ConfigFastest.Valid([]byte(j)))
-	})
+	require.True(t, json.Valid([]byte(j)))
+	require.True(t, jscan.Valid(j))
 }
 
-var gbool bool
+var GB bool
 
 func repeat(s string, n int) string {
 	b := make([]byte, n*len(s))
@@ -438,7 +162,7 @@ func BenchmarkValid(b *testing.B) {
 			return []byte(repeat("[", 1024) + repeat("]", 1024))
 		})},
 		{"unwind_stack__________", SrcMake(func() []byte {
-			return MakeRepeated("[", 1024)
+			return []byte(repeat("[", 1024))
 		})},
 		{"miniscule_1b__________", SrcFile("miniscule_1b.json")},
 		{"tiny_8b_______________", SrcFile("tiny_8b.json")},
@@ -455,74 +179,13 @@ func BenchmarkValid(b *testing.B) {
 			src, err := bd.input.GetJSON()
 			require.NoError(b, err)
 
-			b.Run("jscan___________", func(b *testing.B) {
-				v := jscan.NewValidator[[]byte](1024)
-				b.ResetTimer()
-				for i := 0; i < b.N; i++ {
-					gbool = v.Valid(src)
-				}
-			})
-
-			b.Run("encoding-json___", func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					gbool = json.Valid(src)
-				}
-			})
-
-			b.Run("jsoniter________", func(b *testing.B) {
-				jb := []byte(src)
-				b.ResetTimer()
-				for i := 0; i < b.N; i++ {
-					gbool = jsoniter.Valid(jb)
-				}
-			})
-
-			b.Run("gofaster-jx_____", func(b *testing.B) {
-				jb := []byte(src)
-				d := new(gofasterjx.Decoder)
-				b.ResetTimer()
-				for i := 0; i < b.N; i++ {
-					d.ResetBytes(jb)
-					gbool = d.Validate() != nil
-				}
-			})
-
-			b.Run("tidwallgjson____", func(b *testing.B) {
-				j := string(src)
-				b.ResetTimer()
-				for i := 0; i < b.N; i++ {
-					gbool = tidwallgjson.Valid(j)
-				}
-			})
-
-			b.Run("valyala-fastjson", func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					gbool = (valyalafastjson.ValidateBytes(src) != nil)
-				}
-			})
-
-			b.Run("goccy-go-json___", func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					gbool = goccygojson.Valid(src)
-				}
-			})
-
-			b.Run("bytedance-sonic_", func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					gbool = bytedancesonic.ConfigFastest.Valid(src)
-				}
-			})
+			v := jscan.NewValidator[[]byte](1024)
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				GB = v.Valid(src)
+			}
 		})
 	}
-}
-
-func MakeRepeated(s string, n int) []byte {
-	var b bytes.Buffer
-	b.Grow(len(s) * n)
-	for i := 0; i < n; i++ {
-		b.WriteString(s)
-	}
-	return b.Bytes()
 }
 
 type SourceProvider interface{ GetJSON() ([]byte, error) }
