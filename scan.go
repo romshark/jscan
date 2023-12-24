6fedd3eed14e8b4f1ba8a9ff846c897989335c77
Roman Sharkov <roman.scharkov@gmail.com>
2023-12-24 14:07:59 +0100

    perf: Inline number parser

--- scan.go
+++ scan.go
@@ -1,7 +1,6 @@
 package jscan
 
 import (
-	"github.com/romshark/jscan/v2/internal/jsonnum"
 	"github.com/romshark/jscan/v2/internal/strfind"
 )
 
@@ -165,10 +164,9 @@ func scan[S ~string | ~[]byte](
 	i *Iterator[S], fn func(*Iterator[S]) (err bool),
 ) (S, Error[S]) {
 	var (
-		rollback S // Used as fallback for error report
-		s        = i.src
-		b        bool
-		ks, ke   int
+		s      = i.src
+		b      bool
+		ks, ke int
 	)
 
 VALUE:
@@ -282,12 +280,317 @@ VALUE_ARRAY:
 VALUE_NUMBER:
 	{
 		i.valueIndex = len(i.src) - len(s)
-		{
-			rollback = s
-			if s, b = jsonnum.ReadNumber(s); b {
-				return s, getError(ErrorCodeMalformedNumber, i.src, rollback)
+		var xi int
+
+		if s[0] == '-' {
+			// Signed
+			s = s[1:]
+			if len(s) < 1 {
+				// Expected at least one digit
+				return s, i.getError(ErrorCodeMalformedNumber)
 			}
 		}
+
+		if s[0] == '0' {
+			s = s[1:]
+			if len(s) < 1 {
+				// Zero
+				goto ON_NUM
+			}
+			// Leading zero
+			switch s[0] {
+			case '.':
+				s = s[1:]
+				goto FRACTION
+			case 'e', 'E':
+				s = s[1:]
+				goto EXPONENT_SIGN
+			default:
+				// Zero
+				goto ON_NUM
+			}
+		}
+
+		// Integer
+		if len(s) < 1 || (s[0] < '1' || s[0] > '9') {
+			// Expected at least one digit
+			return s, i.getError(ErrorCodeMalformedNumber)
+		}
+		s = s[1:]
+		for len(s) >= 8 {
+			if s[0] < '0' || s[0] > '9' {
+				if s[0] == 'e' || s[0] == 'E' {
+					s = s[1:]
+					goto EXPONENT_SIGN
+				} else if s[0] == '.' {
+					s = s[1:]
+					goto FRACTION
+				}
+				// Integer
+				goto ON_NUM
+			}
+			if s[1] < '0' || s[1] > '9' {
+				if s[1] == 'e' || s[1] == 'E' {
+					s = s[2:]
+					goto EXPONENT_SIGN
+				} else if s[1] == '.' {
+					s = s[2:]
+					goto FRACTION
+				}
+				// Integer
+				s = s[1:]
+				goto ON_NUM
+			}
+			if s[2] < '0' || s[2] > '9' {
+				if s[2] == 'e' || s[2] == 'E' {
+					s = s[3:]
+					goto EXPONENT_SIGN
+				} else if s[2] == '.' {
+					s = s[3:]
+					goto FRACTION
+				}
+				// Integer
+				s = s[2:]
+				goto ON_NUM
+			}
+			if s[3] < '0' || s[3] > '9' {
+				if s[3] == 'e' || s[3] == 'E' {
+					s = s[4:]
+					goto EXPONENT_SIGN
+				} else if s[3] == '.' {
+					s = s[4:]
+					goto FRACTION
+				}
+				// Integer
+				s = s[3:]
+				goto ON_NUM
+			}
+			if s[4] < '0' || s[4] > '9' {
+				if s[4] == 'e' || s[4] == 'E' {
+					s = s[5:]
+					goto EXPONENT_SIGN
+				} else if s[4] == '.' {
+					s = s[5:]
+					goto FRACTION
+				}
+				// Integer
+				s = s[4:]
+				goto ON_NUM
+			}
+			if s[5] < '0' || s[5] > '9' {
+				if s[5] == 'e' || s[5] == 'E' {
+					s = s[6:]
+					goto EXPONENT_SIGN
+				} else if s[5] == '.' {
+					s = s[6:]
+					goto FRACTION
+				}
+				// Integer
+				s = s[5:]
+				goto ON_NUM
+			}
+			if s[6] < '0' || s[6] > '9' {
+				if s[6] == 'e' || s[6] == 'E' {
+					s = s[7:]
+					goto EXPONENT_SIGN
+				} else if s[6] == '.' {
+					s = s[7:]
+					goto FRACTION
+				}
+				// Integer
+				s = s[6:]
+				goto ON_NUM
+			}
+			if s[7] < '0' || s[7] > '9' {
+				if s[7] == 'e' || s[7] == 'E' {
+					s = s[8:]
+					goto EXPONENT_SIGN
+				} else if s[7] == '.' {
+					s = s[8:]
+					goto FRACTION
+				}
+				// Integer
+				s = s[7:]
+				goto ON_NUM
+			}
+			s = s[8:]
+		}
+		for xi = 0; xi < len(s); xi++ {
+			if s[xi] < '0' || s[xi] > '9' {
+				if s[xi] == 'e' || s[xi] == 'E' {
+					s = s[xi+1:]
+					goto EXPONENT_SIGN
+				} else if s[xi] == '.' {
+					s = s[xi+1:]
+					goto FRACTION
+				}
+				// Integer
+				s = s[xi:]
+				goto ON_NUM
+			}
+		}
+		s = s[xi:]
+
+		if len(s) < 1 {
+			// Integer without exponent
+			goto ON_NUM
+		}
+
+	FRACTION:
+		if len(s) < 1 || (s[0] < '0' || s[0] > '9') {
+			// Expected at least one digit
+			return s, i.getError(ErrorCodeMalformedNumber)
+		}
+		s = s[1:]
+
+		for len(s) >= 8 {
+			if s[0] < '0' || s[0] > '9' {
+				if s[0] == 'e' || s[0] == 'E' {
+					s = s[1:]
+					goto EXPONENT_SIGN
+				}
+				goto ON_NUM
+			}
+			if s[1] < '0' || s[1] > '9' {
+				if s[1] == 'e' || s[1] == 'E' {
+					s = s[2:]
+					goto EXPONENT_SIGN
+				}
+				s = s[1:]
+				goto ON_NUM
+			}
+			if s[2] < '0' || s[2] > '9' {
+				if s[2] == 'e' || s[2] == 'E' {
+					s = s[3:]
+					goto EXPONENT_SIGN
+				}
+				s = s[2:]
+				goto ON_NUM
+			}
+			if s[3] < '0' || s[3] > '9' {
+				if s[3] == 'e' || s[3] == 'E' {
+					s = s[4:]
+					goto EXPONENT_SIGN
+				}
+				s = s[3:]
+				goto ON_NUM
+			}
+			if s[4] < '0' || s[4] > '9' {
+				if s[4] == 'e' || s[4] == 'E' {
+					s = s[5:]
+					goto EXPONENT_SIGN
+				}
+				s = s[4:]
+				goto ON_NUM
+			}
+			if s[5] < '0' || s[5] > '9' {
+				if s[5] == 'e' || s[5] == 'E' {
+					s = s[6:]
+					goto EXPONENT_SIGN
+				}
+				s = s[5:]
+				goto ON_NUM
+			}
+			if s[6] < '0' || s[6] > '9' {
+				if s[6] == 'e' || s[6] == 'E' {
+					s = s[7:]
+					goto EXPONENT_SIGN
+				}
+				s = s[6:]
+				goto ON_NUM
+			}
+			if s[7] < '0' || s[7] > '9' {
+				if s[7] == 'e' || s[7] == 'E' {
+					s = s[8:]
+					goto EXPONENT_SIGN
+				}
+				s = s[7:]
+				goto ON_NUM
+			}
+			s = s[8:]
+		}
+		for xi = 0; xi < len(s); xi++ {
+			if s[xi] < '0' || s[xi] > '9' {
+				if s[xi] == 'e' || s[xi] == 'E' {
+					s = s[xi+1:]
+					goto EXPONENT_SIGN
+				}
+				s = s[xi:]
+				goto ON_NUM
+			}
+		}
+		s = s[xi:]
+
+		if len(s) < 1 {
+			// Number (with fraction but) without exponent
+			goto ON_NUM
+		}
+
+	EXPONENT_SIGN:
+		if len(s) < 1 {
+			// Missing exponent value
+			return s, i.getError(ErrorCodeMalformedNumber)
+		}
+		if s[0] == '-' || s[0] == '+' {
+			s = s[1:]
+		}
+		if len(s) < 1 || (s[0] < '0' || s[0] > '9') {
+			// Expected at least one digit
+			return s, i.getError(ErrorCodeMalformedNumber)
+		}
+		s = s[1:]
+
+		for len(s) >= 8 {
+			if s[0] < '0' || s[0] > '9' {
+				// Number with (fraction and) exponent
+				goto ON_NUM
+			}
+			if s[1] < '0' || s[1] > '9' {
+				// Number with (fraction and) exponent
+				s = s[1:]
+				goto ON_NUM
+			}
+			if s[2] < '0' || s[2] > '9' {
+				// Number with (fraction and) exponent
+				s = s[2:]
+				goto ON_NUM
+			}
+			if s[3] < '0' || s[3] > '9' {
+				// Number with (fraction and) exponent
+				s = s[3:]
+				goto ON_NUM
+			}
+			if s[4] < '0' || s[4] > '9' {
+				// Number with (fraction and) exponent
+				s = s[4:]
+				goto ON_NUM
+			}
+			if s[5] < '0' || s[5] > '9' {
+				// Number with (fraction and) exponent
+				s = s[5:]
+				goto ON_NUM
+			}
+			if s[6] < '0' || s[6] > '9' {
+				// Number with (fraction and) exponent
+				s = s[6:]
+				goto ON_NUM
+			}
+			if s[7] < '0' || s[7] > '9' {
+				// Number with (fraction and) exponent
+				s = s[7:]
+				goto ON_NUM
+			}
+			s = s[8:]
+		}
+		for xi = 0; xi < len(s); xi++ {
+			if s[xi] < '0' || s[xi] > '9' {
+				// Number with (fraction and) exponent
+				s = s[xi:]
+				goto ON_NUM
+			}
+			s = s[xi:]
+		}
+	ON_NUM:
 		i.valueIndexEnd = len(i.src) - len(s)
 		i.valueType = ValueTypeNumber
 
