6ce9b5eb415920b26f24680e2409d1198bd41a4f
Roman Sharkov <roman.scharkov@gmail.com>
2026-07-28 20:18:38 +0200

    chore: Fix linter errors

--- internal/jsonnum/jsonnum.go
+++ internal/jsonnum/jsonnum.go
@@ -50,10 +50,11 @@ func ReadNumber[S ~string | ~[]byte](s S) (trailing S, err ReturnCode) {
 	s = s[1:]
 	for len(s) >= 8 {
 		if s[0] < '0' || s[0] > '9' {
-			if s[0] == 'e' || s[0] == 'E' {
+			switch s[0] {
+			case 'e', 'E':
 				s = s[1:]
 				goto EXPONENT_SIGN
-			} else if s[0] == '.' {
+			case '.':
 				s = s[1:]
 				goto FRACTION
 			}
@@ -61,10 +62,11 @@ func ReadNumber[S ~string | ~[]byte](s S) (trailing S, err ReturnCode) {
 			return s, ReturnCodeInteger
 		}
 		if s[1] < '0' || s[1] > '9' {
-			if s[1] == 'e' || s[1] == 'E' {
+			switch s[1] {
+			case 'e', 'E':
 				s = s[2:]
 				goto EXPONENT_SIGN
-			} else if s[1] == '.' {
+			case '.':
 				s = s[2:]
 				goto FRACTION
 			}
@@ -73,10 +75,11 @@ func ReadNumber[S ~string | ~[]byte](s S) (trailing S, err ReturnCode) {
 			return s, ReturnCodeInteger
 		}
 		if s[2] < '0' || s[2] > '9' {
-			if s[2] == 'e' || s[2] == 'E' {
+			switch s[2] {
+			case 'e', 'E':
 				s = s[3:]
 				goto EXPONENT_SIGN
-			} else if s[2] == '.' {
+			case '.':
 				s = s[3:]
 				goto FRACTION
 			}
@@ -85,10 +88,11 @@ func ReadNumber[S ~string | ~[]byte](s S) (trailing S, err ReturnCode) {
 			return s, ReturnCodeInteger
 		}
 		if s[3] < '0' || s[3] > '9' {
-			if s[3] == 'e' || s[3] == 'E' {
+			switch s[3] {
+			case 'e', 'E':
 				s = s[4:]
 				goto EXPONENT_SIGN
-			} else if s[3] == '.' {
+			case '.':
 				s = s[4:]
 				goto FRACTION
 			}
@@ -97,10 +101,11 @@ func ReadNumber[S ~string | ~[]byte](s S) (trailing S, err ReturnCode) {
 			return s, ReturnCodeInteger
 		}
 		if s[4] < '0' || s[4] > '9' {
-			if s[4] == 'e' || s[4] == 'E' {
+			switch s[4] {
+			case 'e', 'E':
 				s = s[5:]
 				goto EXPONENT_SIGN
-			} else if s[4] == '.' {
+			case '.':
 				s = s[5:]
 				goto FRACTION
 			}
@@ -109,10 +114,11 @@ func ReadNumber[S ~string | ~[]byte](s S) (trailing S, err ReturnCode) {
 			return s, ReturnCodeInteger
 		}
 		if s[5] < '0' || s[5] > '9' {
-			if s[5] == 'e' || s[5] == 'E' {
+			switch s[5] {
+			case 'e', 'E':
 				s = s[6:]
 				goto EXPONENT_SIGN
-			} else if s[5] == '.' {
+			case '.':
 				s = s[6:]
 				goto FRACTION
 			}
@@ -121,10 +127,11 @@ func ReadNumber[S ~string | ~[]byte](s S) (trailing S, err ReturnCode) {
 			return s, ReturnCodeInteger
 		}
 		if s[6] < '0' || s[6] > '9' {
-			if s[6] == 'e' || s[6] == 'E' {
+			switch s[6] {
+			case 'e', 'E':
 				s = s[7:]
 				goto EXPONENT_SIGN
-			} else if s[6] == '.' {
+			case '.':
 				s = s[7:]
 				goto FRACTION
 			}
@@ -133,10 +140,11 @@ func ReadNumber[S ~string | ~[]byte](s S) (trailing S, err ReturnCode) {
 			return s, ReturnCodeInteger
 		}
 		if s[7] < '0' || s[7] > '9' {
-			if s[7] == 'e' || s[7] == 'E' {
+			switch s[7] {
+			case 'e', 'E':
 				s = s[8:]
 				goto EXPONENT_SIGN
-			} else if s[7] == '.' {
+			case '.':
 				s = s[8:]
 				goto FRACTION
 			}
@@ -148,10 +156,11 @@ func ReadNumber[S ~string | ~[]byte](s S) (trailing S, err ReturnCode) {
 	}
 	for i = 0; i < len(s); i++ {
 		if s[i] < '0' || s[i] > '9' {
-			if s[i] == 'e' || s[i] == 'E' {
+			switch s[i] {
+			case 'e', 'E':
 				s = s[i+1:]
 				goto EXPONENT_SIGN
-			} else if s[i] == '.' {
+			case '.':
 				s = s[i+1:]
 				goto FRACTION
 			}
